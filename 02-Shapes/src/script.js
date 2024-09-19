f3cf487d95477a65a81cb5a61a5366ec27eee616
Nimira43 <n.rabone72@aol.com>
2024-09-19 02:10:11 +0100

    Setting up multiple rotating cubes

--- 02-Shapes/src/script.js
+++ 02-Shapes/src/script.js
@@ -2,39 +2,32 @@ import * as THREE from 'three'
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setAnimationLoop(drawFrame)
 document.body.appendChild(renderer.domElement)
 
 const scene = new THREE.Scene()
 const aspect = window.innerWidth / window.innerHeight
 
 const camera = new THREE.PerspectiveCamera(30, aspect)
-camera.position.set(6, 3, 8)
+camera.position.set(3, 3, 8)
 camera.lookAt(new THREE.Vector3())
 
-let geometry = new THREE.BoxGeometry(1, 1, 1 / 3)
-const material = new THREE.MeshPhongMaterial({color: 'red'})
+let geometry = new THREE.BoxGeometry(2, 2,)
+const material = new THREE.MeshPhongMaterial()
 
-let plate
-
-plate = new THREE.Mesh(geometry, material)
-plate.position.x = -1
-scene.add(plate)
-
-plate = new THREE.Mesh(geometry, material)
-plate.position.x = 1
-scene.add(plate)
-
-geometry = new THREE.BoxGeometry(3, 1, 1 / 3)
-
-plate = new THREE.Mesh(geometry, material)
-plate.position.y = 1
-scene.add(plate)
-
-plate = new THREE.Mesh(geometry, material)
-plate.position.y = -1
-scene.add(plate)
+let cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
 
 const light = new THREE.PointLight()
-light.position.set(1, 2, 1)
+light.position.set(3, 4, 5)
 scene.add(light)
-renderer.render(scene, camera)
+
+light = new THREE.PointLight()
+light.position.set(-3, 4, -5)
+scene.add(light)
+
+function drawFrame() {
+  scene.rotation.y += 0.005
+  renderer.render(scene, camera)
+}
+
