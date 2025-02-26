343caa6be8160927ea6f24f0abc4265d77d16226
Nimira43 <n.rabone72@aol.com>
2025-02-26 10:32:46 +0000

    Tried out CircleGeometry.

--- Alpha/main.js
+++ Alpha/main.js
@@ -16,19 +16,29 @@ document.body.appendChild(renderer.domElement)
 // const geometry = new THREE.BoxGeometry(4, 7, 8)
 // const geometry = new THREE.SphereGeometry(7, 9, 38)
 // const geometry = new THREE.PlaneGeometry(12, 5, 21, 17)
-const geometry = new THREE.CylinderGeometry(2, 7, 9, 17, 9, 8)
+// const geometry = new THREE.CylinderGeometry(2, 7, 9, 17, 9, 8)
+// const geometry = new THREE.CircleGeometry(7, 17, 36, 5)
+
+const geometry = new THREE.PlaneGeometry(4, 2, 6, 6)
 const material = new THREE.MeshBasicMaterial({
   color: 0xff4500,
   wireframe: true
 })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+
+const plane = new THREE.Mesh(geometry, material)
+
+
+
+scene.add(plane)
+
+
+
 renderer.render(scene, camera)
 
 function animate() {
   requestAnimationFrame(animate)
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  plane.rotation.x += 0.01
+  plane.rotation.y += 0.01
   renderer.render(scene, camera)
 }
 
