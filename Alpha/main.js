9211c7860fe49f5a8abb697061274211865dadfd
Nimira43 <n.rabone72@aol.com>
2025-02-26 10:01:45 +0000

    Animating cube.

--- Alpha/main.js
+++ Alpha/main.js
@@ -22,3 +22,14 @@ const material = new THREE.MeshBasicMaterial({
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 renderer.render(scene, camera)
+
+function animate() {
+  requestAnimationFrame(animate)
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  renderer.render(scene, camera)
+}
+
+animate()
+
+
