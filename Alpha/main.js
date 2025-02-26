3993a3bf5cf8f6f876b8653873f2b205d400e646
Nimira43 <n.rabone72@aol.com>
2025-02-26 09:28:21 +0000

    Positioned camera. Created renderer and set it's size.

--- Alpha/main.js
+++ Alpha/main.js
@@ -8,4 +8,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 )
+camera.position.set(0, 0, 10)
 
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
