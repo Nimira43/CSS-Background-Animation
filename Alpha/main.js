b9f63987097adabba6003ec287720d9226c1018b
Nimira43 <n.rabone72@aol.com>
2025-02-26 09:54:08 +0000

    Set up cube

--- Alpha/main.js
+++ Alpha/main.js
@@ -13,3 +13,10 @@ camera.position.set(0, 0, 10)
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
+
+const geometry = new THREE.BoxGeometry(2, 2, 2)
+const material = new THREE.MeshBasicMaterial({
+  color: 0xff4500,
+  wireframe: true
+})
+const cube = new THREE.Mesh(geometry, material)
